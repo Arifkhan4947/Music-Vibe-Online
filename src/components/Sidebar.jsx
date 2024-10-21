ba233498073fe9d178aba5ace6f9db349f979fd8
Arif Khan <khanarif2738@gmail.com>
2024-10-21 13:14:07 +0530

    completed seekBg funtionality when someone Click and increase the song duration it will work accordingly.

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import {assets} from '../assets/assets'
 
-const Sidebar = () => {
+const Sidebar = () => {   
+
   return (
     <div className='w-[25%] h-full p-2 flex-col gap-2 text-white hidden lg:flex'>
         <div className='bg-[#121212] h-[15%] rounded flex flex-col justify-around'>
